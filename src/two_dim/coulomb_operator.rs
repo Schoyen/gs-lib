9d00743ae46a27f8f07df971c0713e00831b9b06
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-03-03 16:30:25 +0100

    Bug fix: use com instead of center-diff
        The delta for the I-tilde integrals relates the COM of the Hermite
        Gaussians, not the center difference!
        This commit also includes symmetry tests and a new I-tilde using
        Q-derivative instead of P (see notes).

--- src/two_dim/coulomb_operator.rs
+++ src/two_dim/coulomb_operator.rs
@@ -169,25 +169,23 @@ pub fn construct_coulomb_operator_matrix_element_od(
     let q = od_bd.tot_exp;
 
     let sigma = (p + q) / (4.0 * p * q);
-    let delta = (
-        od_bd.center_diff.0 - od_ac.center_diff.0,
-        od_bd.center_diff.1 - od_ac.center_diff.1,
-    );
+    let delta = (od_bd.com.0 - od_ac.com.0, od_bd.com.1 - od_ac.com.1);
 
     let mut val = 0.0;
 
     for t in 0..(od_ac.x_sum_lim + 1) {
         for u in 0..(od_ac.y_sum_lim + 1) {
-            let e_ac = od_ac.expansion_coefficients(t as i32, u as i32);
+            let e_ac = (-1.0f64).powi((t + u) as i32)
+                * od_ac.expansion_coefficients(t as i32, u as i32);
 
             for tau in 0..(od_bd.x_sum_lim + 1) {
                 for nu in 0..(od_bd.y_sum_lim + 1) {
-                    let e_bd = (-1.0 as f64).powi((tau + nu) as i32)
-                        * od_bd.expansion_coefficients(tau as i32, nu as i32);
+                    let e_bd =
+                        od_bd.expansion_coefficients(tau as i32, nu as i32);
 
                     val += e_ac
                         * e_bd
-                        * int_tilde(
+                        * int_tilde_q(
                             (t + tau) as i32,
                             (u + nu) as i32,
                             sigma,
@@ -203,11 +201,17 @@ pub fn construct_coulomb_operator_matrix_element_od(
         * val
 }
 
-fn int_tilde(t: i32, u: i32, sigma: f64, delta: (f64, f64)) -> f64 {
-    int_tilde_rec(0, t, u, sigma, delta)
+fn int_tilde_q(t: i32, u: i32, sigma: f64, delta: (f64, f64)) -> f64 {
+    int_tilde_rec_q(0, t, u, sigma, delta)
 }
 
-fn int_tilde_rec(n: i32, t: i32, u: i32, sigma: f64, delta: (f64, f64)) -> f64 {
+fn int_tilde_rec_q(
+    n: i32,
+    t: i32,
+    u: i32,
+    sigma: f64,
+    delta: (f64, f64),
+) -> f64 {
     assert!(n >= 0);
 
     if t < 0 || u < 0 {
@@ -218,7 +222,7 @@ fn int_tilde_rec(n: i32, t: i32, u: i32, sigma: f64, delta: (f64, f64)) -> f64 {
         return extended_bessel(n, sigma, delta);
     }
 
-    let mut pre_factor = 1.0 / (8.0 * sigma);
+    let mut pre_factor = -1.0 / (8.0 * sigma);
     let mut val = 0.0;
 
     if n == 0 {
@@ -226,26 +230,26 @@ fn int_tilde_rec(n: i32, t: i32, u: i32, sigma: f64, delta: (f64, f64)) -> f64 {
 
         if t == 0 {
             val += delta.1
-                * (int_tilde_rec(n, t, u - 1, sigma, delta)
-                    + int_tilde_rec(n + 1, t, u - 1, sigma, delta));
+                * (int_tilde_rec_q(n, t, u - 1, sigma, delta)
+                    + int_tilde_rec_q(n + 1, t, u - 1, sigma, delta));
 
             if u > 1 {
-                val += -((u - 1) as f64)
-                    * (int_tilde_rec(n, t, u - 2, sigma, delta)
-                        + int_tilde_rec(n + 1, t, u - 2, sigma, delta));
+                val += ((u - 1) as f64)
+                    * (int_tilde_rec_q(n, t, u - 2, sigma, delta)
+                        + int_tilde_rec_q(n + 1, t, u - 2, sigma, delta));
             }
 
             return val * pre_factor;
         }
 
         val += delta.0
-            * (int_tilde_rec(n, t - 1, u, sigma, delta)
-                + int_tilde_rec(n + 1, t - 1, u, sigma, delta));
+            * (int_tilde_rec_q(n, t - 1, u, sigma, delta)
+                + int_tilde_rec_q(n + 1, t - 1, u, sigma, delta));
 
         if t > 1 {
-            val += -((t - 1) as f64)
-                * (int_tilde_rec(n, t - 2, u, sigma, delta)
-                    + int_tilde_rec(n + 1, t - 2, u, sigma, delta));
+            val += ((t - 1) as f64)
+                * (int_tilde_rec_q(n, t - 2, u, sigma, delta)
+                    + int_tilde_rec_q(n + 1, t - 2, u, sigma, delta));
         }
 
         return val * pre_factor;
@@ -253,30 +257,30 @@ fn int_tilde_rec(n: i32, t: i32, u: i32, sigma: f64, delta: (f64, f64)) -> f64 {
 
     if t == 0 {
         val += delta.1
-            * (int_tilde_rec(n - 1, t, u - 1, sigma, delta)
-                + 2.0 * int_tilde_rec(n, t, u - 1, sigma, delta)
-                + int_tilde_rec(n + 1, t, u - 1, sigma, delta));
+            * (int_tilde_rec_q(n - 1, t, u - 1, sigma, delta)
+                + 2.0 * int_tilde_rec_q(n, t, u - 1, sigma, delta)
+                + int_tilde_rec_q(n + 1, t, u - 1, sigma, delta));
 
         if u > 1 {
-            val += -((u - 1) as f64)
-                * (int_tilde_rec(n - 1, t, u - 2, sigma, delta)
-                    + 2.0 * int_tilde_rec(n, t, u - 2, sigma, delta)
-                    + int_tilde_rec(n + 1, t, u - 2, sigma, delta));
+            val += ((u - 1) as f64)
+                * (int_tilde_rec_q(n - 1, t, u - 2, sigma, delta)
+                    + 2.0 * int_tilde_rec_q(n, t, u - 2, sigma, delta)
+                    + int_tilde_rec_q(n + 1, t, u - 2, sigma, delta));
         }
 
         return val * pre_factor;
     }
 
     val += delta.0
-        * (int_tilde_rec(n - 1, t - 1, u, sigma, delta)
-            + 2.0 * int_tilde_rec(n, t - 1, u, sigma, delta)
-            + int_tilde_rec(n + 1, t - 1, u, sigma, delta));
+        * (int_tilde_rec_q(n - 1, t - 1, u, sigma, delta)
+            + 2.0 * int_tilde_rec_q(n, t - 1, u, sigma, delta)
+            + int_tilde_rec_q(n + 1, t - 1, u, sigma, delta));
 
     if t > 1 {
-        val += -((t - 1) as f64)
-            * (int_tilde_rec(n - 1, t - 2, u, sigma, delta)
-                + 2.0 * int_tilde_rec(n, t - 2, u, sigma, delta)
-                + int_tilde_rec(n + 1, t - 2, u, sigma, delta));
+        val += ((t - 1) as f64)
+            * (int_tilde_rec_q(n - 1, t - 2, u, sigma, delta)
+                + 2.0 * int_tilde_rec_q(n, t - 2, u, sigma, delta)
+                + int_tilde_rec_q(n + 1, t - 2, u, sigma, delta));
     }
 
     val * pre_factor
@@ -330,27 +334,277 @@ mod tests {
         );
     }
 
+    fn int_tilde(t: i32, u: i32, sigma: f64, delta: (f64, f64)) -> f64 {
+        tests::int_tilde_rec(0, t, u, sigma, delta)
+    }
+
+    fn int_tilde_rec(
+        n: i32,
+        t: i32,
+        u: i32,
+        sigma: f64,
+        delta: (f64, f64),
+    ) -> f64 {
+        assert!(n >= 0);
+
+        if t < 0 || u < 0 {
+            return 0.0;
+        }
+
+        if t == 0 && u == 0 {
+            return extended_bessel(n, sigma, delta);
+        }
+
+        let mut pre_factor = 1.0 / (8.0 * sigma);
+        let mut val = 0.0;
+
+        if n == 0 {
+            pre_factor *= 2.0;
+
+            if t == 0 {
+                val += delta.1
+                    * (tests::int_tilde_rec(n, t, u - 1, sigma, delta)
+                        + tests::int_tilde_rec(n + 1, t, u - 1, sigma, delta));
+
+                if u > 1 {
+                    val += -((u - 1) as f64)
+                        * (tests::int_tilde_rec(n, t, u - 2, sigma, delta)
+                            + tests::int_tilde_rec(
+                                n + 1,
+                                t,
+                                u - 2,
+                                sigma,
+                                delta,
+                            ));
+                }
+
+                return val * pre_factor;
+            }
+
+            val += delta.0
+                * (tests::int_tilde_rec(n, t - 1, u, sigma, delta)
+                    + tests::int_tilde_rec(n + 1, t - 1, u, sigma, delta));
+
+            if t > 1 {
+                val += -((t - 1) as f64)
+                    * (tests::int_tilde_rec(n, t - 2, u, sigma, delta)
+                        + tests::int_tilde_rec(n + 1, t - 2, u, sigma, delta));
+            }
+
+            return val * pre_factor;
+        }
+
+        if t == 0 {
+            val += delta.1
+                * (tests::int_tilde_rec(n - 1, t, u - 1, sigma, delta)
+                    + 2.0 * tests::int_tilde_rec(n, t, u - 1, sigma, delta)
+                    + tests::int_tilde_rec(n + 1, t, u - 1, sigma, delta));
+
+            if u > 1 {
+                val += -((u - 1) as f64)
+                    * (tests::int_tilde_rec(n - 1, t, u - 2, sigma, delta)
+                        + 2.0
+                            * tests::int_tilde_rec(n, t, u - 2, sigma, delta)
+                        + tests::int_tilde_rec(n + 1, t, u - 2, sigma, delta));
+            }
+
+            return val * pre_factor;
+        }
+
+        val += delta.0
+            * (tests::int_tilde_rec(n - 1, t - 1, u, sigma, delta)
+                + 2.0 * tests::int_tilde_rec(n, t - 1, u, sigma, delta)
+                + tests::int_tilde_rec(n + 1, t - 1, u, sigma, delta));
+
+        if t > 1 {
+            val += -((t - 1) as f64)
+                * (tests::int_tilde_rec(n - 1, t - 2, u, sigma, delta)
+                    + 2.0 * tests::int_tilde_rec(n, t - 2, u, sigma, delta)
+                    + tests::int_tilde_rec(n + 1, t - 2, u, sigma, delta));
+        }
+
+        val * pre_factor
+    }
+
+    #[test]
+    fn compare_int_tilde_pq() {
+        let mut rng = rand::thread_rng();
+        let mut gaussians = Vec::new();
+
+        for _i in 0..4 {
+            gaussians.push(G2D::new(
+                (rng.gen_range(0..2), rng.gen_range(0..2)),
+                0.5 + rng.gen::<f64>(),
+                (
+                    2.0 * (rng.gen::<f64>() - 0.5),
+                    2.0 * (rng.gen::<f64>() - 0.5),
+                ),
+            ));
+        }
+
+        let mut od_ac = OD2D::new(&gaussians[0], &gaussians[2]);
+        let mut od_bd = OD2D::new(&gaussians[1], &gaussians[3]);
+
+        let p_r = od_ac.tot_exp;
+        let q_r = od_bd.tot_exp;
+
+        let sigma_r = (p_r + q_r) / (4.0 * p_r * q_r);
+        let delta_r = (od_bd.com.0 - od_ac.com.0, od_bd.com.1 - od_ac.com.1);
+
+        for t in 0..(od_ac.x_sum_lim + 1) {
+            for u in 0..(od_ac.y_sum_lim + 1) {
+                let e_ac_q = (-1.0f64).powi((t + u) as i32)
+                    * od_ac.expansion_coefficients(t as i32, u as i32);
+                let e_ac = od_ac.expansion_coefficients(t as i32, u as i32);
+
+                for tau in 0..(od_bd.x_sum_lim + 1) {
+                    for nu in 0..(od_bd.y_sum_lim + 1) {
+                        let e_bd_q =
+                            od_bd.expansion_coefficients(tau as i32, nu as i32);
+                        let e_bd = (-1.0f64).powi((tau + nu) as i32)
+                            * od_bd
+                                .expansion_coefficients(tau as i32, nu as i32);
+
+                        let val_q = e_ac_q
+                            * e_bd_q
+                            * int_tilde_q(
+                                (t + tau) as i32,
+                                (u + nu) as i32,
+                                sigma_r,
+                                delta_r,
+                            );
+                        let val = e_ac
+                            * e_bd
+                            * tests::int_tilde(
+                                (t + tau) as i32,
+                                (u + nu) as i32,
+                                sigma_r,
+                                delta_r,
+                            );
+                        assert_abs_diff_eq!(val_q, val);
+                    }
+                }
+            }
+        }
+    }
+
     #[test]
     fn test_int_tilde() {
-        let delta = (2.3, 0.7);
-        let sigma = 0.6;
+        let mut rng = rand::thread_rng();
+        let mut gaussians = Vec::new();
 
-        assert_abs_diff_eq!(
-            int_tilde(1, 2, sigma, delta),
-            int_tilde(2, 1, sigma, (delta.1, delta.0))
-        );
-        assert_abs_diff_eq!(
-            int_tilde(2, 2, sigma, delta),
-            int_tilde(2, 2, sigma, (delta.1, delta.0))
-        );
-        assert_abs_diff_eq!(
-            int_tilde(1, 2, sigma, delta),
-            -int_tilde(1, 2, sigma, (-delta.0, -delta.1))
-        );
-        assert_abs_diff_eq!(
-            int_tilde(2, 2, sigma, delta),
-            int_tilde(2, 2, sigma, (-delta.0, -delta.1))
-        );
+        for _i in 0..4 {
+            gaussians.push(G2D::new(
+                (rng.gen_range(0..2), rng.gen_range(0..2)),
+                0.5 + rng.gen::<f64>(),
+                (
+                    2.0 * (rng.gen::<f64>() - 0.5),
+                    2.0 * (rng.gen::<f64>() - 0.5),
+                ),
+            ));
+        }
+
+        let mut od_ac = OD2D::new(&gaussians[0], &gaussians[2]);
+        let mut od_bd = OD2D::new(&gaussians[1], &gaussians[3]);
+        let mut od_ca = OD2D::new(&gaussians[2], &gaussians[0]);
+        let mut od_db = OD2D::new(&gaussians[3], &gaussians[1]);
+
+        let p_r = od_ac.tot_exp;
+        let q_r = od_bd.tot_exp;
+        let p_l = od_ca.tot_exp;
+        let q_l = od_db.tot_exp;
+
+        let sigma_r = (p_r + q_r) / (4.0 * p_r * q_r);
+        let sigma_l = (p_l + q_l) / (4.0 * p_l * q_l);
+
+        let delta_rr = (od_bd.com.0 - od_ac.com.0, od_bd.com.1 - od_ac.com.1);
+        let delta_lr = (od_bd.com.0 - od_ca.com.0, od_bd.com.1 - od_ca.com.1);
+        let delta_rl = (od_db.com.0 - od_ac.com.0, od_db.com.1 - od_ac.com.1);
+        let delta_ll = (od_db.com.0 - od_ca.com.0, od_db.com.1 - od_ca.com.1);
+
+        assert_abs_diff_eq!(p_r, p_l);
+        assert_abs_diff_eq!(q_r, q_l);
+        assert_abs_diff_eq!(sigma_r, sigma_l);
+        assert_abs_diff_eq!(delta_rr.0, delta_ll.0);
+        assert_abs_diff_eq!(delta_rr.1, delta_ll.1);
+        assert_abs_diff_eq!(delta_rr.0, delta_lr.0);
+        assert_abs_diff_eq!(delta_rr.1, delta_lr.1);
+        assert_abs_diff_eq!(delta_rr.0, delta_rl.0);
+        assert_abs_diff_eq!(delta_rr.1, delta_rl.1);
+
+        assert!(od_ac.x_sum_lim == od_ca.x_sum_lim);
+        assert!(od_ac.y_sum_lim == od_ca.y_sum_lim);
+        assert!(od_bd.x_sum_lim == od_db.x_sum_lim);
+        assert!(od_bd.y_sum_lim == od_db.y_sum_lim);
+
+        let mut val_rr = 0.0;
+        let mut val_lr = 0.0;
+        let mut val_rl = 0.0;
+        let mut val_ll = 0.0;
+
+        for t in 0..(od_ac.x_sum_lim + 1) {
+            for u in 0..(od_ac.y_sum_lim + 1) {
+                let tu_sign = (-1.0f64).powi((t + u) as i32);
+                let e_ac =
+                    tu_sign * od_ac.expansion_coefficients(t as i32, u as i32);
+                let e_ca =
+                    tu_sign * od_ca.expansion_coefficients(t as i32, u as i32);
+
+                assert_abs_diff_eq!(e_ac, e_ca);
+
+                for tau in 0..(od_bd.x_sum_lim + 1) {
+                    for nu in 0..(od_bd.y_sum_lim + 1) {
+                        let e_bd =
+                            od_bd.expansion_coefficients(tau as i32, nu as i32);
+                        let e_db =
+                            od_db.expansion_coefficients(tau as i32, nu as i32);
+
+                        assert_abs_diff_eq!(e_bd, e_db);
+
+                        val_rr += e_ac
+                            * e_bd
+                            * int_tilde_q(
+                                (t + tau) as i32,
+                                (u + nu) as i32,
+                                sigma_r,
+                                delta_rr,
+                            );
+                        val_lr += e_ca
+                            * e_bd
+                            * int_tilde_q(
+                                (t + tau) as i32,
+                                (u + nu) as i32,
+                                sigma_r,
+                                delta_lr,
+                            );
+                        val_rl += e_ac
+                            * e_db
+                            * int_tilde_q(
+                                (t + tau) as i32,
+                                (u + nu) as i32,
+                                sigma_r,
+                                delta_rl,
+                            );
+                        val_ll += e_ca
+                            * e_db
+                            * int_tilde_q(
+                                (t + tau) as i32,
+                                (u + nu) as i32,
+                                sigma_l,
+                                delta_ll,
+                            );
+
+                        assert_abs_diff_eq!(val_rr, val_ll);
+                        assert_abs_diff_eq!(val_rr, val_lr);
+                        assert_abs_diff_eq!(val_rr, val_rl);
+                    }
+                }
+            }
+        }
+
+        assert_abs_diff_eq!(val_rr, val_ll);
+        assert_abs_diff_eq!(val_rr, val_lr);
+        assert_abs_diff_eq!(val_rr, val_rl);
     }
 
     #[test]
@@ -362,7 +616,7 @@ mod tests {
             G2D::new((1, 1), 1.0, (-1.0, 0.3)),
         ];
 
-        let u = construct_coulomb_operator_matrix_elements(&gaussians);
+        let u = tests::construct_coulomb_operator_matrix_elements(&gaussians);
         let l = gaussians.len();
 
         assert!(u.shape().len() == 4);
@@ -376,6 +630,9 @@ mod tests {
                 for r in 0..l {
                     for s in 0..l {
                         assert_abs_diff_eq!(u[[p, q, r, s]], u[[q, p, s, r]]);
+                        assert_abs_diff_eq!(u[[p, q, r, s]], u[[r, q, p, s]]);
+                        assert_abs_diff_eq!(u[[p, q, r, s]], u[[p, s, r, q]]);
+                        assert_abs_diff_eq!(u[[p, q, r, s]], u[[r, s, p, q]]);
                     }
                 }
             }
