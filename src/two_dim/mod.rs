c5a8e4054be64ce499659a42b888b0990f218c76
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-03-09 11:19:37 +0100

    Add L_z operator matrix elements

--- src/two_dim/mod.rs
+++ src/two_dim/mod.rs
@@ -1,6 +1,7 @@
 mod g2d;
 mod od2d;
 
+mod angular_moment;
 mod coulomb_operator;
 mod differential_operator;
 mod multipole_moment;
@@ -8,6 +9,7 @@ mod multipole_moment;
 pub use g2d::G2D;
 pub use od2d::OD2D;
 
+pub use angular_moment::construct_angular_moment_z_matrix_elements;
 pub use coulomb_operator::construct_coulomb_operator_matrix_elements;
 pub use differential_operator::{
     construct_differential_operator_matrix_elements,
